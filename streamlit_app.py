4af79738da1889360b2688c98211a6e9e2fe8b51
Settphong04 <Sattpong04@gmail.com>
2025-05-27 11:12:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import cv2
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.subheader("แปลงภาพ Color ไปเป็นภาพ Grayscale")
+img_file = st.file_uploader("เปิดไฟล์ภาพ")
+
+if img_file is not None:    
+    file_bytes = np.asarray(bytearray(img_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    #------------------------------------------------
+    img_out = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
+    #------------------------------------------------
+    col1, col2 = st.columns(2)
+    col1.image(img, caption='ภาพ Color',channels="BGR")
+    col2.image(img_out, caption='ภาพ Grayscale')
